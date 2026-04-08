aab5d57b426775f126a201ac63dde4a340287c26
Ashray Desai <ashraydesai28@gmail.com>
2026-04-07 21:49:56 -0700

    Started learing Fastapi and what is pydantic

---
+++ blog/schemas.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from typing import List
+
+# In show only inherit tht parent class if any logic is there or else you always import the base model and see for the config according to the version.
+
+
+
+
+class BlogBase(BaseModel):
+    title : str
+    body : str
+
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name : str
+    email : str
+    password : str
+
+class ShowUser(BaseModel):
+    name : str
+    email : str
+
+    blogs : List[Blog]
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title : str
+    body : str
+
+
+    creator : ShowUser
+    class Config():
+        orm_mode= True
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+from typing import Optional
+# from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,7 +9,35 @@ app = FastAPI()
 def index():
     return {'data':{'name':'Ashray'}}
 
+@app.get('/blog')
+def about(limit = 10 ,published: bool = True ,sort: Optional[str] = None):
+    if published == True:
+        # only get 10 published blog
+        return {'data': f'{limit} published blogs from db'}
+    else:
+        return {'data':f"{limit} blogs from db"}
 
-@app.get('/about')
-def about():
-    return {'data':'hey'}
+@app.get('/blog/unpublished')
+def show():
+    # fetch blog with id = id 
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    # fetch blog with id = id 
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+     # fetch blog with id = id 
+     return {'data': {'1','2'}}
+
+class Blog(BaseModel):
+    title : str
+    body  : str
+    published_at : Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    
+    return {'data':f'blog is created with title as {blog.title}'}
